8ee70eb07a404110fc74d1058273a07ec77dc5b9
udarrr <sergey.klyshnikov2@gmail.com>
2021-01-22 00:48:57 +0300

    Java F Optional Task#2 (1-2)

---
+++ Java F Optional Task#2/src/com/company/CreateNewMassive.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CreateNewMassive {
+
+    int[][] arr()
+    {
+        int n = 0;
+        Random count = new Random();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < arr.length; ++i) {
+            for(int j = 0; j < arr[i].length; ++j) {
+                arr[i][j] = count.nextInt(9);
+            }
+        }
+
+        return arr;
+    }
+
+    int[][] copyOf(int[][] original)
+    {
+        int[][] copy = new int[original.length][];
+        for (int i = 0; i < original.length; i++)
+        {
+            copy[i] = Arrays.copyOf(original[i],original.length);
+        }
+        return copy;
+    }
+}
---
+++ Java F Optional Task#2/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter n: ");
+        CreateNewMassive cn = new CreateNewMassive();
+        int[][] newArr = cn.arr();
+        int[][] initialArr = cn.copyOf(newArr);
+        System.out.println("Unsorted array is: ");
+
+        for(int i = 0; i < newArr.length; i++) {
+             System.out.println(Arrays.toString(newArr[i]));
+        }
+
+        SortOfArrayInt sort = new SortOfArrayInt();
+        sort.SortOfNumbers(newArr);
+        System.out.println("Sorted array is: ");
+
+         for(int i = 0; i< newArr.length; i++)
+         {
+            System.out.println(Arrays.toString(newArr[i]));
+         }
+
+        ArrayMethods meth = new ArrayMethods();
+        System.out.println("The longest sequences: " + Arrays.toString(meth.SearchSequences(initialArr)));
+    }
+}
