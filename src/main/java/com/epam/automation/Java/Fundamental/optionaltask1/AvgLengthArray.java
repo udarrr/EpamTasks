f0434405d9b56c64190625370b133ed8b5fa7e5c
udarrr <sergey.klyshnikov2@gmail.com>
2021-01-31 23:42:41 +0300

    Java.Fundamental

---
+++ src/main/java/com/epam/automation/Java/Fundamental/optionaltask1/AvgLengthArray.java
@@ -0,0 +1,75 @@
+package com.epam.automation.Java.Fundamental.optionaltask1;
+
+public class AvgLengthArray {
+
+        String[] AvgHalfOfLengthLess(String[] numbers) {  //Task #3
+        int avgLength = 0;
+        int countNumbersLess =0;
+
+        for(int i = 0; i < numbers.length;i++)
+        {
+            avgLength+=numbers[i].length();
+        }
+        SortOfArrayNumbers avgRes = new SortOfArrayNumbers();
+        numbers = avgRes.SortOfNumbers(numbers);
+
+        for(int i = 0; i < numbers.length;i++)
+        {
+            if(numbers[i].length() <= avgLength/numbers.length)
+            {
+                countNumbersLess++;
+            }
+        }
+        String[] lessHalfResult = new String[countNumbersLess];
+
+        for(int i = 0; i < numbers.length;i++) {
+            if (numbers[i].length() <= avgLength / numbers.length)
+            {
+                lessHalfResult[i] = numbers[i];
+            }
+        }
+        return  lessHalfResult;
+    }
+
+    String[] AvgHalfOfLengthMore(String[] numbers) {
+        int avgLength = 0;
+        int countNumbersMore =0;
+
+        for(int i = 0; i < numbers.length;i++)
+        {
+            avgLength+=numbers[i].length();
+        }
+        SortOfArrayNumbers avgRes = new SortOfArrayNumbers();
+        numbers = avgRes.SortOfNumbers(numbers);
+
+        for(int i = 0; i < numbers.length;i++)
+        {
+            if(numbers[i].length() > avgLength/numbers.length)
+            {
+                countNumbersMore++;
+            }
+        }
+        String[] MoreHalfResult = new String[countNumbersMore];
+        int count = 0; //чтобы не превысить размерность масива MoreHalfResult = countNumbersMore
+
+        for(int i = 0; i < numbers.length;i++) {
+            if (numbers[i].length() > avgLength / numbers.length && count < countNumbersMore)
+            {
+                MoreHalfResult[count] = numbers[i];
+                ++count;
+            }
+        }
+        return  MoreHalfResult;
+    }
+
+    String[] LengthOfArrayNumbers(String[] numbers) {
+
+        String[] lengthArr = new String[numbers.length];
+
+        for(int i = 0; i < numbers.length;i++)
+        {
+            lengthArr[i] =String.valueOf(numbers[i].length());
+        }
+        return  lengthArr;
+    }
+}
