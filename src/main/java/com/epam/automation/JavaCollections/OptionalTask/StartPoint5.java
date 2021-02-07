48edfa090dd3e630ebf6b6b7fdf0163000ffdce5
udarrr <sergey.klyshnikov2@gmail.com>
2021-02-07 14:44:19 +0300

    Java.Collections

---
+++ src/main/java/com/epam/automation/JavaCollections/OptionalTask/StartPoint5.java
@@ -0,0 +1,37 @@
+package com.epam.automation.JavaCollections.OptionalTask;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class StartPoint5
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(-1);
+        list.add(-2);
+        list.add(1);
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        list.add(-5);
+        list.add(-4);
+        list.add(-3);
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(-6);
+        list.add(-8);
+        list.add(-9);
+        list.add(-7);
+        list.add(6);
+        list.add(0);
+
+        list.sort((Comparator<Integer>) (o1,o2) -> -(o1-o2));
+
+        System.out.println(list);
+    }
+}
