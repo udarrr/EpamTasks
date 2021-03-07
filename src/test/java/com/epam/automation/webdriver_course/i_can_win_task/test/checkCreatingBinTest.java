d7777539d3125c3511d96eb999baec43332f7c87
udarrr <sergey.klyshnikov2@gmail.com>
2021-03-07 13:27:19 +0300

    WebDriver (i can win)

--- src/test/java/com/epam/automation/webdriver_course/i_can_win_task/test/checkCreatingBinTest.java
+++ src/test/java/com/epam/automation/webdriver_course/i_can_win_task/test/checkCreatingBinTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class NewPastCreatorTest {
+public class checkCreatingBinTest {
     private ChromeDriver driver;
 
     @BeforeMethod(alwaysRun = true)
@@ -22,7 +22,7 @@ public class NewPastCreatorTest {
         boolean expectedCreatedOneNewPastBin = new ThePasteBinHomePage(driver)
                 .openPage()
                 .pasteBin("Hello from WebDriver")
-                .selectExpirationTime(ExpirationTime.MINUTES_10.getExpirationTime())
+                .selectExpirationTime(ExpirationTime.MINUTES_10.getExpiration())
                 .pasteNameTitle("helloweb")
                 .createNewBin()
                 .checkResult();
