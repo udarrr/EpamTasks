99f899bfcaab2254abe4559f0564fc39d66de067
udarrr <sergey.klyshnikov2@gmail.com>
2021-03-30 19:32:16 +0300

    JavaIO path fix

--- src/main/java/com/epam/automation/javaio_stage2/maintask/resources/PathPlaces.java
+++ src/main/java/com/epam/automation/javaio_stage2/maintask/resources/PathPlaces.java
@@ -1,7 +1,7 @@
 package com.epam.automation.javaio_stage2.maintask.resources;
 
 public class PathPlaces {
-    private static final String placeAfterReadingDirectories = "src\\main\\java\\com\\epam\\automation\\JavaIO\\MainTask\\Files\\file.txt";
+    private static final String placeAfterReadingDirectories = "src\\main\\java\\com\\epam\\automation\\javaio_stage2\\maintask\\files\\file.txt";
 
     public static String getPlaceAfterReadingDirectories() {
         return placeAfterReadingDirectories;
